a77ad24a940c65ac8615e500a68ea84398fd8de0
Evgenij Ivankin <e.ivankin@innopolis.university>
2022-06-16 01:48:25 +0300

    fix dependencies and some settings

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wall -fno-warn-type-defaults #-}
+{-# OPTIONS_GHC -fdefer-typed-holes -fshow-hole-constraints -funclutter-valid-hole-fits #-}
 module Main where
 
 import Minesweeper
