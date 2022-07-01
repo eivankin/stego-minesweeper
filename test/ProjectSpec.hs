8cde28718ee65ce5236f268b2a8a33501a1eedee
Evgenij Ivankin <e.ivankin@innopolis.university>
2022-07-01 22:12:45 +0300

    fix test description

--- test/ProjectSpec.hs
+++ test/ProjectSpec.hs
@@ -7,7 +7,7 @@ import System.Random
 
 spec :: Spec
 spec = do
-  describe "Message operations" $ do
+  describe "Message encoding / decoding" $ do
     let msg = "MINESWEEPER"
     it "Check if decodeMessage . encodeMessage == id" $
       decodeMessage (encodeMessage (mkStdGen 42) msg) == msg
